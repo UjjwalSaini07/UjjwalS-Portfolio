b5f4a1399fe7203b49016ae8faca4ad1af7c13d7
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2024-07-16 12:22:20 +0530

    Bug Fix in Init 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Preloader from './components/Preloader';
+import Preloader from './components/Preloader.js';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
