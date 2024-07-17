45663218897c326b8ac55bce648d0af03372cd49
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2024-07-17 12:27:35 +0530

    Adding Sidebar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Route, Routes } from "react-router-dom";
 
 import Preloader from './components/Preloader.js';
 import AppLayout from "./components/AppLayout";
+import SideBar from './components/Sidebar/SideBar.jsx'
+
 import Home from './Pages/Home.js';
 import About from './Pages/About.js';
 import Skills from './Pages/Skills.js';
@@ -24,18 +26,21 @@ const App = () => {
     <div  className="App">
       {loading ? <Preloader /> : (
       <>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/work" element={<WorkEx />} />
-            <Route path="/projects" element={<Project />} />
-            <Route path="/contact" element={<Contact />} />
-          </Route>
-          <Route path="*" element={<Error404 />} />
-          {/* <Route path="*" element={<h1>404 Not Found</h1>} /> */}
-        </Routes>
+        {/* <SideBar /> */}
+        <SideBar>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/skills" element={<Skills />} />
+              <Route path="/work" element={<WorkEx />} />
+              <Route path="/projects" element={<Project />} />
+              <Route path="/contact" element={<Contact />} />
+            </Route>
+            <Route path="*" element={<Error404 />} />
+            {/* <Route path="*" element={<h1>404 Not Found</h1>} /> */}
+          </Routes>
+        </SideBar>
       </>
     )}
     </div>
