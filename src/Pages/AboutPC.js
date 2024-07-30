f0f42693c8e6789aad3f066522679a74aacc7831
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2024-07-30 11:07:52 +0530

    Web Fix Try2

--- src/Pages/AboutPC.js
+++ src/Pages/AboutPC.js
@@ -96,8 +96,8 @@ const aboutMeStyle = `
 `;
 
 const ParticlularImageStyle = {
-  width: '35rem',
-  height: '46.5rem',
+  // width: '35rem',
+  // height: '46.5rem',
   // border: '1px solid black', // Optional, to visualize the container
   display: 'flex',
 
