7be1cbb17209b2ed807d78995a6dfd6f13b17b5d
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2024-08-17 23:49:37 +0530

    Add Animated List

--- src/Pages/CompoPage/HomeP3.js
+++ src/Pages/CompoPage/HomeP3.js
@@ -1,52 +1,122 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
-import { useSpring, animated } from '@react-spring/web';
+import {AnimatedListDemo} from './AnimatedListDemo';
 
-const HomePcContainer = {
+const homePcContainer = {
   position: "absolute",
   width: "100%",
   height: "100vh",
   overflow: "hidden",
-  zindex: "1",
+  zIndex: 1,
 };
 
-const spline_model = {
+const splineModel = {
   position: "absolute",
   top: "0",
   left: "0",
   width: "100%",
   height: "100%",
-  zIndex: "1",
+  zIndex: 1,
 };
 
-const division_overlay = {
+const rightSideContainer = {
   position: "absolute",
   top: "20px",
-  width: "62%",
+  right: "30px",
+  height: "68%",
+  width: "26%",
   fontSize: "large",
-  left: "30px",
-  zIndex: "5",
+  zIndex: 5,
   color: "rgb(240, 2, 149)",
   background: "transparent",
-  padding: "10px",
+  // background: "#ff3636",
+  padding: "4px",
+  marginTop: "120px",
   borderRadius: "8px",
-}
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const spaceboardsFont = `
+  @font-face {
+    font-family: 'Spaceboards';
+    src: url('/fonts/Spaceboards.otf') format('opentype');
+  }
+`;
+
+const Home3Style = `
+  .home3 {
+    font-family: 'Spaceboards', sans-serif;
+    font-size: 3rem;
+    font-weight: bold;
+    background: linear-gradient(90deg, #ff7f00, #ff0000, #ff7f00, #ff0000);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: gradient 1.5s infinite;
+    letter-spacing: 0.1rem;
+    background-size: 200% 200%;
+  }
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+
+  @keyframes glow {
+    0% {
+      text-shadow: 
+        0 0 0 rgba(255, 127, 0, 0.7), 
+        0 0 0 rgba(255, 127, 0, 0.7), 
+        0 0 0 rgba(255, 0, 0, 0.7), 
+        0 0 0 rgba(255, 0, 0, 0.7), 
+        0 0 0 rgba(255, 127, 0, 0.5), 
+        0 0 0 rgba(255, 0, 0, 0.5);
+    }
+    100% {
+      text-shadow: 
+        0 0 1px rgba(255, 127, 0, 1), 
+        0 0 2px rgba(255, 127, 0, 1), 
+        0 0 5px rgba(255, 0, 0, 1), 
+        0 0 8px rgba(255, 0, 0, 1), 
+        0 0 12px rgba(255, 127, 0, 0.7), 
+        0 0 15px rgba(255, 0, 0, 0.7);
+    }
+  }
+`;
 
 function Home() {
-  
-  return (
-    <div style={HomePcContainer}>
-      <Spline style={spline_model} scene="https://prod.spline.design/yF0tNNwe0CsgeJWs/scene.splinecode" />
-      {/* This is a Iron Suited Man Pointed towards the polarized side */}
-      {/* <Spline style={spline_model} scene="https://prod.spline.design/e-hwKBI8zlLCial1/scene.splinecode" /> */}
-      <div style={division_overlay}>   
-          <div>
+  useEffect(() => {
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = spaceboardsFont + Home3Style;
+    document.head.appendChild(styleElement);
+    
+    return () => {
+      document.head.removeChild(styleElement);
+    };
+  }, []);
 
-          </div>
+  return (
+    <div style={homePcContainer}>
+      <Spline style={splineModel} scene="https://prod.spline.design/yF0tNNwe0CsgeJWs/scene.splinecode" />
+      {/* This is an Iron Suited Man Pointed towards the polarized side */}
+      {/* <Spline style={splineModel} scene="https://prod.spline.design/e-hwKBI8zlLCial1/scene.splinecode" /> */}
+      <div style={rightSideContainer}>  
+        <div className="home3" style={{ textAlign: 'center'}}>
+          Show-Casing
+        </div> 
+        <AnimatedListDemo />
       </div>
     </div>
   );
 }
 
 export default Home;
-
