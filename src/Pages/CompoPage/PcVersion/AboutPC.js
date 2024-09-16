26db6c427a311d7f3cf57539955e642f4c5bf3bf
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2024-09-16 18:19:50 +0530

    update styling of AboutPC

--- src/Pages/CompoPage/PcVersion/AboutPC.js
+++ src/Pages/CompoPage/PcVersion/AboutPC.js
@@ -164,12 +164,12 @@ const About = () => {
     }} 
     id="aboutPC">
       
-      <Spline 
+      {/* <Spline 
         style={spline_model} 
         scene="https://prod.spline.design/tQH4xs3CwWIS7EtM/scene.splinecode"
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-      />
+      /> */}
 
       <div style={{ marginTop: '4rem' }}></div>
 
@@ -179,7 +179,7 @@ const About = () => {
         boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
         border: '1px solid rgba(255, 255, 255, 0.3)',
         padding: '20px',
-        maxWidth: '1300px',
+        maxWidth: '1380px',
         minHeight: '720px',
         width: '100%',
         margin: '5px auto',
@@ -205,18 +205,18 @@ const About = () => {
         </div>
         <div className="content">
         <motion.div
-            variants={textVariant(0.1)}
+            variants={textVariant(0.6)}
             initial="hidden"
             animate="show"
         >
-          <h3 style={{fontFamily: "'Great Vibes', cursive", color: '#ffffff', fontSize: '4rem'}}>I'm Ujjwal Saini</h3>
+          <h3 style={{fontFamily: "'Great Vibes', cursive", color: '#ffffff', fontSize: '4.6rem'}}>I'm Ujjwal Saini</h3>
           <span style={{
-              fontSize: '2rem', color: '#ff00ff', fontWeight: 600, fontFamily: "'Vidaloka', serif", marginTop: '1rem',
+              fontSize: '2.2rem', color: '#ff00ff', fontWeight: 600, fontFamily: "'Vidaloka', serif", marginTop: '1rem',
           }}>
             Full Stack Developer | Designer </span>
           </motion.div>
           <motion.div
-            variants={textVariant(0.6)}
+            variants={textVariant(0.9)}
             initial="hidden"
             animate="show"
         >
@@ -227,61 +227,73 @@ const About = () => {
                 fontFamily: "'Open Sans', sans-serif",
                 fontStyle: 'italic',
                 fontWeight: 400,
-                lineHeight: 1.7,
+                lineHeight: 1.5,
               }}>
-                I am Ujjwal, an IT Engineering student passionate about computers and aiming to kickstart a career in Software Development and Web Development. Currently based in Delhi, India, I am pursuing my undergraduate studies in Information Technology at GGSIPU. My enthusiasm lies in enhancing my coding abilities and crafting applications and websites. As Well-organised & collaborative individual,<br/> I thrive in team environemnts and enjoy bringing inovative solutions to table.<br/><br/> Awesome!! Let's Build the Next Big Thing...
+                I am Ujjwal, an IT Engineering student passionate about computers and aiming to kickstart a career in Software Development and Web Development. Currently based in Delhi, India, I am pursuing my undergraduate studies in Information Technology at GGSIPU. My enthusiasm lies in enhancing my coding abilities and crafting applications and websites. As Well-organised & collaborative individual.<br/> I thrive in team environemnts and enjoy bringing inovative solutions to table.<br/> Awesome!! Let's Build the Next Big Thing...
           </p>
           </motion.div>
           
           <div className="box-container">
             <div className="box">
-            <motion.div
-                variants={textVariant(0.9)}
-                initial="hidden"
-                animate="show"
-            >
+              <motion.div variants={textVariant(1.4)} initial="hidden" animate="show">
               <p><i className="bi bi-person-badge-fill" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
               <span style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Age : </span> 21</p>
+              </motion.div>
+              <motion.div variants={textVariant(1.8)} initial="hidden" animate="show">
               <p><i className="bi bi-telephone-fill" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
               <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Phone : </span>
               <a href="tel:+919717899079" target="_blank" rel="noopener noreferrer" style={{ color: '#fcfcfc' }}>+91 97178-99079</a></p>
+              </motion.div>
+              <motion.div variants={textVariant(2.2)} initial="hidden" animate="show">
               <p><i className="bi bi-cake-fill" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
               <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Birthday : </span> 7th Feburary</p>
               </motion.div>
             </div>
             <div className="box">
-            <motion.div
-                variants={textVariant(1.2)}
-                initial="hidden"
-                animate="show"
-            >
+              <motion.div variants={textVariant(2.6)} initial="hidden" animate="show">
+              <p><i className="bi bi-globe" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
+              <span style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Nationality : </span> Indian</p>
+              </motion.div>
+              <motion.div variants={textVariant(3)} initial="hidden" animate="show">
+              <p><i className="bi bi-mortarboard-fill" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
+              <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Degree : </span> BTech- IT'26</p>
+              </motion.div>
+              <motion.div variants={textVariant(3.4)} initial="hidden" animate="show">
+              <p><i className="bi bi-bank2" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
+              <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> University : </span> GGSIPU</p>
+              </motion.div>
+            </div>
+            <div className="box">
+              <motion.div variants={textVariant(3.8)} initial="hidden" animate="show">
               <p><i className="bi bi-envelope-at-fill" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
               <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Email : </span>
               <a href="mailto:ujjwalsaini0007@gmail.com" target="_blank" rel="noopener noreferrer" style={{ color: '#fcfcfc' }}>ujjwalsaini0007@gmail.com</a></p>
+              </motion.div>
+              <motion.div variants={textVariant(4.2)} initial="hidden" animate="show">
               <p><i className="bi bi-geo-alt-fill" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
               <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Place : </span> Delhi, India - 110015</p>
-              <p><i className="bi bi-globe" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
+              </motion.div>
+              <motion.div variants={textVariant(4.5)} initial="hidden" animate="show">
+              <p><i className="bi bi-translate" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
               <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Language Known : </span> English, Hindi, Punjabi</p>
               </motion.div>
             </div>
             <div className="box">
-            <motion.div
-                variants={textVariant(1.5)}
-                initial="hidden"
-                animate="show"
-            >
+              <motion.div variants={textVariant(4.9)} initial="hidden" animate="show">
               <p><i className="bi bi-star-fill" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
-              <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Hobbies : </span> Cube Solving, Gardening, Learning New SKills, Travelling, Listening Music</p>
+              <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Hobbies : </span> Cube Solving, Gardening, Learning New Skills, Travelling, Listening Music, Playing Football</p>
+              </motion.div>
+              <motion.div variants={textVariant(5.2)} initial="hidden" animate="show">
               <p><i className="bi bi-linkedin" style={{ fontSize: '1.7rem', color: '#ff00ff', marginRight: '0.5rem' }}></i>
               <span  style={{color: '#ff00ff', fontFamily: "'Vidaloka', serif", fontWeight: 500, marginTop: '1rem'}}> Linkedin : </span>
-              <a href="https://www.linkedin.com/in/ujjwalsaini07/" target="_blank" rel="noopener noreferrer" style={{ color: isHovered ?  '#fcfcfc' : '#17fc03', pointerEvents: 'auto',}}>Connect Here</a></p>
+              <a href="https://www.linkedin.com/in/ujjwalsaini07/" target="_blank" rel="noopener noreferrer" style={{ color: isHovered ?  '#fcfcfc' : '#17fc03', pointerEvents: 'auto' }}>Connect Here</a></p>
               </motion.div>
             </div>
           </div>
           
           <div className="glowbtnAbt">
           <motion.div
-                variants={textVariant(1.8)}
+                variants={textVariant(6)}
                 initial="hidden"
                 animate="show"
             >
